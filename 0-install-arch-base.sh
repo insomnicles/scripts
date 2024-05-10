6e4bb25403961af3fbce2a4023a87aa080b93451
insomnicles <13597665+insomnicles@users.noreply.github.com>
2024-05-10 01:08:18 -0400

    update

--- 0-install-arch-base.sh
+++ 0-install-arch-base.sh
@@ -5,6 +5,7 @@
 #              base X11 packages
 #    roughly follows the Installation Guide 
 #
+
 UEFI_MODE=0
 ARCH_VERSION=2024-05-01
 
@@ -13,43 +14,24 @@ PART_ROOT_SIZE=5G
 PART_SWAP_SIZE=1G
 PART_HOME_SIZE=2G
 
-HOSTNAME=
-
 TIME_ZONE="Canada/Eastern"
 LOCALE="en_US.UTF-8 UTF-8"
-BASE_PACS="base linux linux-firmware sof-firmware iwd man-db man-pages texinfo neovim vi base-devel sudo pacman-contrib intel-ucode grub efibootmgr git openssh zsh"
+
+BASE_PACS="base linux linux-firmware sof-firmware util-linux iwd man-db man-pages texinfo neovim vi base-devel sudo pacman-contrib intel-ucode grub efibootmgr git openssh zsh"
 X11_PACS="ttf-dejavu gnu-free-fonts xorg-server xorg-xinit xf86-input-libinput xorg-server-common xorg-xclipboard xterm xclip dmenu i3-wm xfce4-terminal firefox"
+
 SYSTEMD_ENABLED="iwd systemd-resolved systemd-timesyncd sshd"
 
 greeting() {
 
   cat <<EOF
 
-Run script by: 
-bash <(curl -s https://raw.githubusercontent.com/insomnicles/scripts/main/0-install-base.sh)  
+Welcome to Arch ${ARCH_VERSION} OS Installation.
 
------------------------------------------------------------------
-
-Hi, Arch Linux Install Begins.
-
-ARCH VERSION: ${ARCH_VERSION}
-
-Requirements
-  1. Bootable USB with ARCH iso
-  2. Boot to USB ***in UEFI mode**
-  3. partitioned disk using fdisk as follows:
-        /dev/sda1[nvme0n1p1]   1Gb    UEFI          /boot
-        /dev/sda2[nvme0n1p2]   25%    Linux Ext 4   / 
-        /dev/sda3[nvme0n1p3]   4-8Gb  Linux Swap 
-        /dev/sda4[nvme0n1p4]   75%    Linux Ext 4   /home
-
-  Continue only if the above are satisfied.
-
-  Press any key to continue. Ctrl-C to exit.
+Press any key to continue. Ctrl-C to exit.
 
 EOF
 read cont
-
 }
 
 boot_mode() {
@@ -76,7 +58,7 @@ get_network_inputs() {
     export ARCH_WIFI_NETWORK_PASSWD=${inp_wifi_passwd}
   fi
 
-  printf "\n\nEnter hostname:"
+  printf "\nEnter hostname:"
   read inp_hostname
   export ARCH_HOSTNAME=${inp_hostname}
 }
@@ -86,7 +68,7 @@ config_wifi() {
      iwctl station wlan0 connect ${ARCH_WIFI_NETWORK} -p ${ARCH_WIFI_NETWORK_PASSWD}
   fi
 
-  echo -e "\n\nTesting internet connection..."
+  echo -e "\nTesting internet connection..."
   $(ping -c 3 archlinux.org &>/dev/null) || (echo "Not Connected to Network!!!" && exit 1)
   echo "Good!  We're connected!!!" && sleep 3
 }
@@ -97,38 +79,65 @@ create_partitions(){
   if [ "${UEFI_MODE}" -eq 1 ]; then
     lsblk 
     cat <<"EOF"
-Which partition do you want to setup: 
-nvme0n1p [Desktop]
-sda      [Laptop]
+Enter the device you want to Install Arch on:
+e.g. nvme0n1 or sda
 
 Type One of the above exactly: 
 EOF
     read IN_DEVICE
     IN_DEVICE=/dev/${IN_DEVICE}
 
-  #  if [[ $IN_DEVICE =~ nvme ]]; then
-  #    $PART_EFI_DEVICE="${IN_DEVICE}p1"   # NOT for MBR systems
-  #   ROOT_DEVICE="${IN_DEVICE}p2"  # only for non-LVM
-  #   SWAP_DEVICE="${IN_DEVICE}p3"  # only for non-LVM 
-  #   HOME_DEVICE="${IN_DEVICE}p4"  # only for non-LVM
-  # else
-  #   EFI_DEVICE="${IN_DEVICE}1"   # NOT for MBR systems
-  #   ROOT_DEVICE="${IN_DEVICE}2"  # only for non-LVM
-  #   SWAP_DEVICE="${IN_DEVICE}3"  # only for non-LVM 
-  #   HOME_DEVICE="${IN_DEVICE}4"  # only for non-LVM
-  # fi
+    DEVICE_SIZE=sfdisk -s $IN_DEVICE
+    DEVICE_MB=`expr ${DEVICE_SIZE} / 2195312   5048`
+    DEVICE_GB=`expr ${DEVICE_MB} / 1000`
+    DEVICE_SIZE=`lsblk | grep ^sda | awk '{ print $1 $4 }' | cut -d G -f 1`
+
+    MEM_SIZE_GB=`free -g -h -t | grep Mem | awk '{print $2}' |cut -dG -f 1`
+    MEM_SIZE_KB=`cat /proc/meminfo |grep MemTotal | cut -d ":" -f 2 | cut -d "k" -f 1 | awk '{$1=$1};1'`  # in KB / 343
+    MIN_ROOT_GB=8
+    MAX_ROOT_GB=100
+
+    $PART_UEFI_SIZE=1G
+    $PART_SWAP_SIZE=`expr $MEM_SIZE_GB * 1.5`"G"
+
+    $DEVICE_LEFT=`expr $MEM_ - $MEM`
+    if [[ "$DEVICE_GB" -lt "$MIN_ROOT" ]]; then
+      echo "Insufficient Space"
+      exit
+    else if [[ "$DEVICE_GB" -gt "$MAX_ROOT_GB" ]]; then
+      $PART_ROOT_SIZE=$MAX_ROOT_SIZE
+    else 
+      $PART_ROOT_SIZE=$MAX_ROOT_SIZE
+    fi
+    # ROOT: min 4G; max: 50G
+    # HOME: LEFT
+
+    if [[ $IN_DEVICE =~ nvme ]]; then
+      $PART_UEFI="${IN_DEVICE}p1"
+      $PART_ROOT="${IN_DEVICE}p2"
+      $PART_SWAP="${IN_DEVICE}p3" 
+      $PART_HOME="${IN_DEVICE}p4"
+    else if [[ $IN_DEVICE == 'sda' ]]; then
+      $PART_UEFI="${IN_DEVICE}1"
+      $PART_ROOT="${IN_DEVICE}2"
+      $PART_SWAP="${IN_DEVICE}3"  
+      $PART_HOME="${IN_DEVICE}4" 
+    else
+      echo "Device not recognized" && exit
+    fi
 
     sgdisk -Z "$IN_DEVICE"
     sgdisk -n 1::+"$PART_UEFI_SIZE" -t 1:ef00 -c 1:EFI "$IN_DEVICE"
     sgdisk -n 2::+"$PART_ROOT_SIZE" -t 2:8300 -c 2:ROOT "$IN_DEVICE"
     sgdisk -n 3::+"$PART_SWAP_SIZE" -t 3:8200 -c 3:SWAP "$IN_DEVICE"
-    sgdisk -n 4::+"$PART_HOME_SIZE" -t 3:8300 -c 4:HOME "$IN_DEVICE"
-    #sgdisk -n 4 -c 4:HOME "$IN_DEVICE"
+    sgdisk -n 4 -c 4:HOME "$IN_DEVICE"
+    #sgdisk -n 4::+"$PART_HOME_SIZE" -t 3:8300 -c 4:HOME "$IN_DEVICE"
   fi 
 
-   mkfs.fat -F 32 ${IN_DEVICE}1             # formatting efi boot partition
-   mkfs.ext4 ${IN_DEVICE}2                  # formatting root partition
-   mkswap ${IN_DEVICE}3                     # creating swap partition
+   mkfs.fat -F 32 ${IN_DEVICE}1
+   mkfs.ext4 ${IN_DEVICE}2
+   mkswap ${IN_DEVICE}3
+   mkfs.ext4 ${IN_DEVICE}4
 
    mount --mkdir ${IN_DEVICE}2 /mnt         # mounting /
    mount --mkdir ${IN_DEVICE}1 /mnt/boot    # mounting /boot
@@ -142,7 +151,11 @@ install_base_packages() {
 }
 
 config_network() {
-  echo $ARCH_HOSTNAME > /mnt/etc/hostname
+  cat <<EOF > /mnt/etc/hostname
+$ARCH_HOSTNAME
+localhost   127.0.0.1
+EOF
+
   if [ "${WLAN}" -eq 1 ]; then 
     cat << "EOF" > /mnt/etc/systemd/network/25-wireless.network
 [Match]
@@ -172,7 +185,6 @@ config_time() {
 }
 
 config_locale() {
-  #sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /mnt/etc/locale.gen
   sed -i 's/#${LOCALE}/${LOCALE}/' /mnt/etc/locale.gen
   arch-chroot /mnt locale-gen
   echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
@@ -180,7 +192,6 @@ config_locale() {
 
 install_x11_packages() {
   arch-chroot /mnt pacman -S --noconfirm ${X11_PACS}
-  #ttf-dejavu gnu-free-fonts xorg-server xorg-xinit xf86-input-libinput xorg-server-common xorg-xclipboard xterm xclip dmenu i3-wm xfce4-terminal firefox
 }
 
 config_users() {
@@ -194,13 +205,17 @@ config_users() {
   read inp_username
   arch-chroot /mnt useradd -m -G wheel -s /bin/bash ${inp_username}
 
-  printf "\n\nUser Password\n"
+  printf "\nUser Password\n"
   arch-chroot /mnt passwd ${inp_username}
 }
 
 config_systemd() {
   printf "\nSetting up Systemd\n"
   arch-chroot /mnt systemctl enable ${SYSTEMD_ENABLED}
+}
+
+kernel_modules() {
+
 }
 
 install_bootloader() {
@@ -216,7 +231,6 @@ bye() {
   cat <<"EOF"
 
 Installation complete!
-
 - remove USB 
 - reboot the system or type reboot
 
@@ -228,29 +242,19 @@ install_arch_base() {
    boot_mode
    get_network_inputs
    config_wifi
-
    create_partitions
-
    install_base_packages
-   
    config_network
-
    config_time
-   
    config_locale
-
    install_x11_packages
-
    config_users
-
    config_systemd
-
+   kernel_modules
    install_bootloader
-
    cleanup
-
    bye
 }
 
-install_arch_base 2> ./install-error.log | tee ./install-output.log 
+install_arch_base 2> ./install-error.log 
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# scripts
+# Scripts
 
 A script for a minimal Arch OS installation following the installation guide 
 
@@ -9,15 +9,6 @@ A script for a minimal Arch OS installation following the installation guide
 # Installation
 
 1. Boot ArchOS from USB in **UEFI MODE** 
-2. iwctl station wlan0 get-networks
-3. iwctl station wlan0 connect "your wifi network"
-4. fdisk /dev/sda 
-    Partition Drive; e.g..
-        /dev/sda1[nvme0n1p1]   1Gb    UEFI          /boot
-        /dev/sda2[nvme0n1p2]   25%    Linux Ext 4   / 
-        /dev/sda3[nvme0n1p3]   4-8Gb  Linux Cache   
-        /dev/sda4[nvme0n1p4]   75%    Linux Ext 4   /home
-5. Run 
 ```
   $ bash <(curl -s https://raw.githubusercontent.com/insomnicles/scripts/main/0-install-arch-base-stage-1.sh)
 ```
